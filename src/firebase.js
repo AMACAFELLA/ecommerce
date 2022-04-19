4c4741c126131f92122f541a8aa8381755ffb543
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-04-19 15:00:20 +0200

    Updated files
    Working on header.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5FiSvgjcwziykfzIAXP8udyUwiaGTThU",
+    authDomain: "ecommerce-18956.firebaseapp.com",
+    projectId: "ecommerce-18956",
+    storageBucket: "ecommerce-18956.appspot.com",
+    messagingSenderId: "569631527493",
+    appId: "1:569631527493:web:c603caa2f8f9083018092c",
+    measurementId: "G-14KR70GRV4"
+  };
